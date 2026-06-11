21509934ea94f15ac26a022a1ac517fc534caeb3
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-11 23:19:57 +0200

    Can view the full image

--- ImageViewer.c
+++ ImageViewer.c
@@ -10,29 +10,31 @@ int main () {
   fgets(pthrowaway, 1000, in);
   //read comment line 
   fgets(pthrowaway, 1000, in);
-  free(pthrowaway);
   //read width and height
   char *pdimensions = calloc(1000, sizeof(char));
   fgets(pdimensions, 1000, in);
-  
   int width = -1;
   int height = -1;
   sscanf(pdimensions, "%d %d\n", &width, &height);
+  free(pdimensions);
+  //reads the max color value
+  fgets(pthrowaway, 1000, in);
+  free(pthrowaway);
 
-  printf("width: %d, height %d\n", width, height);
 
   SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-  Uint8 r,g,b;
-  r = 0xFF;
-  g=b=0x00;
-  Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
-
+  Uint32 color = 0;
 
   SDL_Rect pixel = (SDL_Rect){0,0,1,1};
-  for (int x=0; x<width; x++) {
-    for (int y=0; y<height; y++) {
+  for (int y=0; y<height; y++) {
+    for (int x=0; x<width; x++) {
+      Uint8 r,g,b;
+      r = (char) getchar();
+      g = (char) getchar();
+      b = (char) getchar();
+      color = SDL_MapRGB(psurface->format,r,g,b);
       pixel.x = x;
       pixel.y = y;
       SDL_FillRect(psurface, &pixel, color);
