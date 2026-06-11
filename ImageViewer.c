a331c14e746d59434ccd68e73e0149be760fa2a0
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-11 23:04:16 +0200

    Reading the width and height of the ppm file

--- ImageViewer.c
+++ ImageViewer.c
@@ -3,11 +3,24 @@
 #include <SDL2/SDL.h>
 
 int main () {
+  
+  FILE *in = stdin;
+  char *pthrowaway = calloc(1000, sizeof(char));
+  //read first line (specifier P3 or P6) ignored
+  fgets(pthrowaway, 1000, in);
+  //read comment line 
+  fgets(pthrowaway, 1000, in);
+  free(pthrowaway);
+  //read width and height
+  char *pdimensions = calloc(1000, sizeof(char));
+  fgets(pdimensions, 1000, in);
+  
+  int width = -1;
+  int height = -1;
+  sscanf(pdimensions, "%d %d\n", &width, &height);
 
-  const int width = 900;
-  const int height = 600;
+  printf("width: %d, height %d\n", width, height);
 
-  printf("hellow world\n");
   SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
@@ -15,6 +28,7 @@ int main () {
   r = 0xFF;
   g=b=0x00;
   Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
+
 
   SDL_Rect pixel = (SDL_Rect){0,0,1,1};
   for (int x=0; x<width; x++) {
