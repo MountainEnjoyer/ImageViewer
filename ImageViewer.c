ae7bf6177f0efbba5ac21eda249f3dd31082a38c
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-11 22:18:44 +0200

    painting each pixel

--- ImageViewer.c
+++ ImageViewer.c
@@ -3,8 +3,12 @@
 #include <SDL2/SDL.h>
 
 int main () {
+
+  const int width = 900;
+  const int height = 600;
+
   printf("hellow world\n");
-  SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, 0);
+  SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
   Uint8 r,g,b;
@@ -12,10 +16,14 @@ int main () {
   g=b=0x00;
   Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
 
-  int x = 50;
-  int y = 50;
-  SDL_Rect pixel = (SDL_Rect){x,y,1,1};
-  SDL_FillRect(psurface, &pixel, color);
+  SDL_Rect pixel = (SDL_Rect){0,0,1,1};
+  for (int x=0; x<width; x++) {
+    for (int y=0; y<height; y++) {
+      pixel.x = x;
+      pixel.y = y;
+      SDL_FillRect(psurface, &pixel, color);
+    }
+  }
   SDL_UpdateWindowSurface(pwindow);
   SDL_Delay(3000);
 
